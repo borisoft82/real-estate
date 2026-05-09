d9b35d71241791ef9661a80089cca591504a8357
borisoft82 <borisoft82@gmail.com>
2026-05-09 13:04:22 +0200

    user seeder added

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +14,33 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('users')->insert([
+            [
+                'name' => 'Admin',
+                'username' => 'admin',
+                'email' => 'admin@testbpz.com',
+                'password' => Hash::make('password'),
+                'role' => 'admin',
+                'status' => '1'
+            ],
+
+            [
+                'name' => 'Agent',
+                'username' => 'agent',
+                'email' => 'agent@testbpz.com',
+                'password' => Hash::make('password'),
+                'role' => 'agent',
+                'status' => '1'
+            ],
+
+            [
+                'name' => 'User',
+                'username' => 'user',
+                'email' => 'user@testbpz.com',
+                'password' => Hash::make('password'),
+                'role' => 'user',
+                'status' => '1'
+            ]
+        ]);
     }
 }
